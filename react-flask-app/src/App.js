8e202d3bca0a9c173c6f348851d4caaf6a2b4fc0
byyhong <wzry24680@gmail.com>
2023-06-19 22:36:42 -0500

    Implemented test cases

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,12 +1,23 @@
 import LineGraph from "./components/LineGraph.jsx"
 import React from 'react'
 import Boxplot from "./components/Boxplot.jsx"
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+import { ErrorBoundary } from "react-error-boundary";
+
 function App() {
+    const queryClient = new QueryClient();
   return (
-    <div className="App">
-        <LineGraph />
-        <Boxplot />
-    </div>
+    <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <LineGraph />
+                <Boxplot />
+            </div>
+        </QueryClientProvider>
+    </ErrorBoundary>
   );
 }
 
--- react-flask-app/src/App.test.js
+++ react-flask-app/src/App.test.js
@@ -1,8 +1,51 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { ErrorBoundary } from "react-error-boundary";
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
+import 'whatwg-fetch';
+import DatePicker from 'react-datepicker'; // Import DatePicker
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const server = setupServer(
+    rest.get('http://localhost:5000/prediction',  async (req,res,ctx) => {
+        return res(ctx.json([['2023-01-03', 70], ['2023-01-04', 80]]));
+
+    }),
+    rest.get('http://localhost:5000/prediction/:date',  async (req,res,ctx) => {
+        const date =  await req.params.date;
+        return res(ctx.json([[date, 70], [date, 80]]));
+
+    }),
+);
+
+beforeAll(() => {
+  // Start the mock server before running tests
+  server.listen();
 });
+afterAll(() => {
+  // Clean up the mock server after running tests
+
+  server.close();
+});
+test('Test text', async () => {
+    const {container} = render(
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+            <App />
+        </ErrorBoundary>
+   )
+    await fetch('http://localhost:5000/prediction/2023-01-04');
+    expect(screen.getByText(/Predicted Death Cases Range:/i)).toBeInTheDocument();
+    screen.debug(container, { maxDepth: 20 });
+});
+
+test('Test date-picker', async () => {
+    const {container} = render(
+        <ErrorBoundary fallback={<div>Something went wrong</div>}>
+            <App />
+        </ErrorBoundary>
+   )
+
+    await fetch('http://localhost:5000/prediction/2023-01-04');
+    expect(screen.getByRole('textbox')).toHaveValue('01/02/2023');
+});
+
